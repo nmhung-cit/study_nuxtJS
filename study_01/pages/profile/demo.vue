02921693684a53f7df74df514c94eafb9020017f
nmhung <hung-nm@if-vietnam.com>
2023-03-02 15:43:36 +0700

    demo nuxt-child

---
+++ study_01/pages/profile/demo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+    <h1>this is chidl demo</h1>
+    <nuxt-child />
+    </div>
+</template>
---
+++ study_01/pages/profile/demo/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>test nuxt-child</h2>
+    </div>
+</template>
